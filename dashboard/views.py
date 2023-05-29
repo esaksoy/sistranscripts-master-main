14f0ebcaf8737da5d0b671656cca834f5f38e450
ebakay <134390405+ebakay@users.noreply.github.com>
2023-05-29 15:15:40 +0300

    fix generate pdf functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login as auth_login
 from django.views.decorators.csrf import csrf_exempt
 from . import forms as dashboard_forms
+<<<<<<< HEAD
 from .models import Student, Score
+=======
+from .models import Student, Score, Class
+>>>>>>> 48a3010 (fix generate pdf functionality)
 from django.http import HttpResponse
 from reportlab.pdfgen import canvas
 
@@ -28,12 +32,20 @@ def detail(request, id=None):
     context = {'object': obj, 'classes': classes, "form": year_form}
     return render(request, 'dashboard/detail.html', context)
 
+<<<<<<< HEAD
 def grades_pdf(request):
+=======
+def grades_pdf(request, id):
+        
+    if request.method == 'POST':
+        
+>>>>>>> 48a3010 (fix generate pdf functionality)
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="grades_report.pdf"'
 
         p = canvas.Canvas(response)
 
+<<<<<<< HEAD
         year = request.POST.get('year')
 
         student_id = request.POST.get('id')
@@ -47,11 +59,31 @@ def grades_pdf(request):
         for class_obj in classes:
             scores = Score.objects.filter(studentid=obj, classes_taken=class_obj)
             p.drawString(100, 100, f"Class: {class_obj.classname}")
+=======
+        # year = request.POST.get('year')
+
+        student_id = id
+        obj = get_object_or_404(Student, pk=student_id)
+        classes = request.POST.getlist('courses[]')
+        print(classes)
+
+        for class_obj in classes:
+            scores = Score.objects.filter(studentid=obj, classes_taken=class_obj)
+            class_name = Class.objects.get(classid=class_obj).classname
+
+            print(class_obj)
+
+            p.drawString(100, 100, f"Class: {class_name}")
+>>>>>>> 48a3010 (fix generate pdf functionality)
             p.drawString(100, 120, "Scores:")
             y_position = 140
 
             for score in scores:
+<<<<<<< HEAD
                 p.drawString(120, y_position, f"Subject: {Score.classes_taken.classname}")
+=======
+                p.drawString(120, y_position, f"Subject: {Score.classes_taken}")
+>>>>>>> 48a3010 (fix generate pdf functionality)
                 p.drawString(120, y_position + 20, f"Score: {Score.score}")
                 y_position += 40
 
